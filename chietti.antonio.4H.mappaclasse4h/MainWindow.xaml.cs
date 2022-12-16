e6155c7e41986479621e9b0ca4bdc200d796ce1f
Antonio Chietti <116791293+antoniochietti@users.noreply.github.com>
2022-12-16 09:51:39 +0100

    prima versione

---
+++ chietti.antonio.4H.mappaclasse4h/MainWindow.xaml.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace chietti.antonio._4H.mappaclasse4h
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();   
+
+        }
+
+        private void Button_Click16(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Banco di Francesco Priori");
+        }
+
+        private void Button_Click17(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Banco di Davide Capicchioni");
+        }
+
+        private void Button_Click18(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Banco di Alessandro Burioli");
+        }
+
+        private void Button_Click9(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Banco di Leo Dyrmyshi");
+        }
+
+        private void Button_Click8(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Banco di Antonio Chietti");
+        }
+
+        private void Button_Click7(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Banco di Karol Basilico");
+        }
+
+        private void Button_Click6(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Banco di Matteo Tumidei ");
+        }
+
+        private void Button_Click5(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Banco di  Joseph Ricci");
+        }
+
+        private void Button_Click4(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Banco di Fatih Islamovski");
+        }
+
+        private void Button_Click3(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Banco di Federico Zhang");
+        }
+
+        private void Button_Click2(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Banco di Giovanni Morelli");
+        }
+
+        private void Button_Click1(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Banco di Federico Casadei");
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Banco di Johan Ghinelli");
+        }
+
+        private void Button_Click14(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Banco di Francesco Pompa");
+        }
+
+        private void Button_Click13(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Banco di Matteo Salvi");
+        }
+
+        private void Button_Click12(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Banco di Samuele Lombardi");
+        }
+
+        private void Button_Click11(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Banco di Federico Cau");
+        }
+
+        private void Button_Click10(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Banco di Alessandro Capanna");
+        }
+    }
+}
